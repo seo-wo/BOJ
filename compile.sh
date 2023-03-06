b22eb9011297df10c37993969768659701f028d0
Seowoo Kim <seowokim@c1r7s1.42seoul.kr>
2023-03-06 20:31:23 +0900

    change simply

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Put problem's number to compile"
-read number
-
+# echo "Put problem's number to compile"
+# read number
+number=${1}
 c++ \#${number}/${number}.cpp -o test
 echo "Compiled problem ${number} has been done"
