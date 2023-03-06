01089274a0c0b0ac6643330859e7fef7bb8834ba
Seowoo Kim <seowokim@c1r7s1.42seoul.kr>
2023-03-06 19:38:51 +0900

    10172 solved

---
+++ #10172/10172.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+	std::string dog = "|\\_/|\n"
+					  "|q p|   /}\n"
+					  "( 0 )\"\"\"\\\n"
+					  "|\"^\"`    |\n"
+					  "||_/=\\\\__|";
+	std::cout << dog;
+}
