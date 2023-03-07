cca17ece351ee55136cd87d30aaca03a2fdeafa8
Seowoo Kim <seowokim@c1r9s7.42seoul.kr>
2023-03-07 15:53:34 +0900

    25083 solved

---
+++ #25083/25083.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+	std::string ss = "         ,r\'\"7\n"
+				"r`-_   ,\'  ,/\n"
+				" \\. \". L_r\'\n"
+				"   `~\\/\n"
+				"      |\n"
+				"      |";
+	std::cout << ss;
+}
