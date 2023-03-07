d76ce0f4a393fde5f44f8db265706147c46a777c
Seowoo Kim <seowokim@c1r9s7.42seoul.kr>
2023-03-07 15:32:11 +0900

    5597 solved

---
+++ #5597/5597.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(void)
+{
+	int list[31] = {0,};
+	int i(0), num(0);
+	while (++i <= 28)
+	{
+		std::cin >> num;
+		list[num] = 1;
+	}
+	i = 0;
+	int cnt(0);
+	while (++i < 31)
+	{
+		if (list[i] == 0 && cnt == 0)
+		{
+			std::cout << i << '\n';
+			++cnt;
+		}
+		else if (list [i] == 0)
+			std::cout << i;
+	}
+}
