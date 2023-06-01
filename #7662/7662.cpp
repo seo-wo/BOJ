8541dd6d180481f9adbe3f7d158462e1f5a2a717
김서우 <4nyth4me@gmail.com>
2023-06-01 15:45:09 +0900

    7662 solved

--- #7662/7662.cpp
+++ #7662/7662.cpp
@@ -1,37 +1,59 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+#include <map>
+#define MAX 2147483648
+#define MIN -2147483649
 
 inline void fun(int k){
-	int n(0), I(0), D(0);
+	int n(0);
 	char c(0);
-	std::priority_queue<int> dq1;
-	std::priority_queue<int, std::vector<int>, std::greater<int> > dq2;
+	std::map<int, int> m;
+	long min(MAX), max(MIN);
 
 	while(k){
 		std::cin >> c >> n;
 		if (c == 'I'){
-			++I;
-			dq1.push(n);
-			dq2.push(n);
-		}
-		else if (c == 'D'){
-			if (dq1.empty() || dq2.empty()) ;
-			else{
-				++D;
-				if (n == -1)
-					dq2.pop();
-				else if (n == 1)
-					dq1.pop();
+			++m[n];
+			if (n <= min)
+				min = n;
+			if (n >= max)
+				max = n;
+		} else if (c == 'D'){
+			if (n == 1){
+				if (m.find(max) != m.end()){
+					--m[max];
+					if (m[max] == 0){
+						m.erase(max);
+						if (m.empty()){
+							max = MIN;
+							min = MAX;
+						} else{
+							std::map<int, int>::reverse_iterator it = m.rbegin();
+							max = it->first;
+						}
+					}
+				}
+			} else if (n == -1){
+				if (m.find(min) != m.end()){
+					--m[min];
+					if (m[min] == 0){
+						m.erase(min);
+						if (m.empty()){
+							max = MIN;
+							min = MAX;
+						} else{
+							min = m.begin()->first;
+						}
+					}
+				}
 			}
 		}
 		--k;
 	}
 
-	if (D >= I)
+	if (m.empty())
 		std::cout << "EMPTY\n";
-	else
-		std::cout << dq1.top() << ' ' << dq2.top() << '\n';
+	else 
+		std::cout << max << ' ' << min << '\n';
 }
 
 int main(void){
